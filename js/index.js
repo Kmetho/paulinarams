2971a0d4d1ccf24d7055556bd7b53daecc7c1259
Kmetho <wercche@gmail.com>
2023-01-18 23:38:07 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+let wzium = document.getElementById('wzium');
+let incrX = -1;
+let incrY = 1;
 
+function letsGO() {
+
+};
+
+function handleCollision() {
+
+};
+
+function move() {
+wzium.style.top = wzium.offsetTop + incrY;
+wzium.style.left = wzium.offsetLeft + incrX;
+};
+
+letsGO();
