03070d4c31d1326c84482027f029d0f653811e2a
Kmetho <wercche@gmail.com>
2023-01-20 01:56:27 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 const area = document.querySelector(".homepage");
 const wzium = document.querySelector("#wzium");
+const wzz = { x: 30, y: 30, ani: {}, hovered: false };
+wzium.style.positon = "absolute";
+wzium.style.right = "${b.x}px";
+wzium.style.top = "${b.y}px";
 
-
-wzium
-
+wzium.addEventListener("mouseenter", () => {
+  if (!b.hovered) {
+    b.ani = requestAnimationFrame(move);
+  } else {
+  }
+});
 
 const spin = document.querySelector(".work-click");
 spin.onmouseenter = function () {
