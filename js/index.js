6764d09eaa2cfbce81b74fc227250ad371032986
Kmetho <wercche@gmail.com>
2023-01-20 19:47:06 +0100

    MOVE WORK WHOP WHOP

--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
 const area = document.querySelector(".homepage");
+const spin = document.querySelector(".work-click");
 const wzium = document.querySelector("#wzium");
-const wzz = { x: 30, y: 30, ani: {}, hovered: false };
+
+const w = {
+  x: 30,
+  y: 30,
+  dx: 1,
+  dy: 1,
+  ani: {},
+  isMoving: false,
+};
+
 wzium.style.positon = "absolute";
-wzium.style.right = "${b.x}px";
-wzium.style.top = "${b.y}px";
+wzium.style.right = w.x + "px";
+wzium.style.top = w.y + "px";
 
 wzium.addEventListener("mouseenter", () => {
-  if (!b.hovered) {
-    b.ani = requestAnimationFrame(move);
+  if (!w.isMoving) {
+    w.ani = requestAnimationFrame(mover);
+    w.isMoving = true;
+    spin.classList.remove("hovered");
   } else {
+    cancelAnimationFrame(w.ani);
+    w.isMoving = false;
+    spin.classList.add("hovered");
   }
 });
 
-const spin = document.querySelector(".work-click");
-spin.onmouseenter = function () {
-  this.classList.add("hovered");
-};
-spin.onmouseleave = function () {
-  this.classList.remove("hovered");
-};
+function mover() {
+  w.x += w.dx;
+  w.y += w.dy;
+  wzium.style.right = w.x + "px";
+  wzium.style.top = w.y + "px";
+  if (w.isMoving) {
+    w.ani = requestAnimationFrame(mover);
+  }
+}
