d67daf0bb408fabbe9cc658d965963f0d7a9e02d
Kmetho <wercche@gmail.com>
2023-01-21 17:42:06 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ const wzium = document.querySelector("#wzium");
 const w = {
   x: 30,
   y: 30,
-  dx: 1,
-  dy: 1,
+  dx: 0.5,
+  dy: 0.5,
   ani: {},
   isMoving: true,
 };
@@ -16,6 +16,7 @@ let areaHeight = window.innerHeight;
 wzium.style.positon = "absolute";
 wzium.style.right = w.x + "px";
 wzium.style.top = w.y + "px";
+
 w.ani = requestAnimationFrame(mover);
 
 wzium.addEventListener("mouseenter", () => {
@@ -23,7 +24,7 @@ wzium.addEventListener("mouseenter", () => {
   w.isMoving = false;
 });
 wzium.addEventListener("mouseleave", () => {
-  w.ani = requestAnimationFrame(mover);
+  setTimeout((w.ani = requestAnimationFrame(mover)), 3000);
   w.isMoving = true;
 });
 
@@ -32,13 +33,19 @@ function mover() {
   w.y += w.dy;
   wzium.style.right = w.x + "px";
   wzium.style.top = w.y + "px";
-
   if (w.y > areaHeight - 53 || w.y < 0) {
     w.dy *= -1;
   }
   if (w.x > areaWidth - 129.483 || w.x < 0) {
     w.dx *= -1;
   }
-
   w.isMoving & (w.ani = requestAnimationFrame(mover));
 }
+
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + e.pageY + "px; left: " + e.pageX + "px;"
+  );
+});
