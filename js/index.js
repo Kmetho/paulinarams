854c2bce1d6d2136eb5a8e073a3de3530e1376fb
Kmetho <wercche@gmail.com>
2023-01-30 17:28:11 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -49,3 +49,11 @@ document.addEventListener("mousemove", (e) => {
     "top: " + e.pageY + "px; left: " + e.pageX + "px;"
   );
 });
+
+if (areaHeight < 690) {
+  const icons = document.querySelectorAll("i");
+  icons.forEach((element) => {
+    element.classList.remove("fa-2xl");
+    element.classList.add("fa-xl");
+  });
+}
