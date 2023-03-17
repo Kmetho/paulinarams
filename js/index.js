365c29be3ee441ad71614a274dd8c854a53391f5
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-03-17 16:27:19 +0100

    Add sick cursor

--- js/index.js
+++ js/index.js
@@ -1,50 +1,56 @@
-const area = document.querySelector(".homepage");
-const wzium = document.querySelector("#wzium");
+// const area = document.querySelector(".homepage");
+// const wzium = document.querySelector("#wzium");
 
-const w = {
-  x: 30,
-  y: 30,
-  dx: 0.75,
-  dy: 0.75,
-  ani: {},
-  isMoving: true,
-};
+// const w = {
+//   x: 30,
+//   y: 30,
+//   dx: 0.75,
+//   dy: 0.75,
+//   ani: {},
+//   isMoving: true,
+// };
 
-let areaWidth = window.innerWidth;
-let areaHeight = window.innerHeight;
+// let areaWidth = window.innerWidth;
+// let areaHeight = window.innerHeight;
 
-wzium.style.positon = "absolute";
-wzium.style.right = w.x + "px";
-wzium.style.top = w.y + "px";
+// wzium.style.positon = "absolute";
+// wzium.style.right = w.x + "px";
+// wzium.style.top = w.y + "px";
 
-w.ani = requestAnimationFrame(mover);
+// w.ani = requestAnimationFrame(mover);
 
-wzium.addEventListener("mouseenter", () => {
-  cancelAnimationFrame(w.ani);
-  w.isMoving = false;
-});
-wzium.addEventListener("mouseleave", () => {
-  setTimeout((w.ani = requestAnimationFrame(mover)), 3000);
-  w.isMoving = true;
-});
+// wzium.addEventListener("mouseenter", () => {
+//   cancelAnimationFrame(w.ani);
+//   w.isMoving = false;
+// });
+// wzium.addEventListener("mouseleave", () => {
+//   setTimeout((w.ani = requestAnimationFrame(mover)), 3000);
+//   w.isMoving = true;
+// });
 
-function mover() {
-  w.x += w.dx;
-  w.y += w.dy;
-  wzium.style.right = w.x + "px";
-  wzium.style.top = w.y + "px";
-  if (w.y > areaHeight - 53 || w.y < 0) {
-    w.dy *= -1;
-  }
-  if (w.x > areaWidth - 129.483 || w.x < 0) {
-    w.dx *= -1;
-  }
-  w.isMoving & (w.ani = requestAnimationFrame(mover));
-}
+// function mover() {
+//   w.x += w.dx;
+//   w.y += w.dy;
+//   wzium.style.right = w.x + "px";
+//   wzium.style.top = w.y + "px";
+//   if (w.y > areaHeight - 53 || w.y < 0) {
+//     w.dy *= -1;
+//   }
+//   if (w.x > areaWidth - 129.483 || w.x < 0) {
+//     w.dx *= -1;
+//   }
+//   w.isMoving & (w.ani = requestAnimationFrame(mover));
+// }
 
-const cursor = document.querySelector(".cursor");
+const cursorB = document.querySelector(".big");
+const cursorS = document.querySelector(".small");
+
 document.addEventListener("mousemove", (e) => {
-  cursor.setAttribute(
+  cursorB.setAttribute(
+    "style",
+    "top: " + e.pageY + "px; left: " + e.pageX + "px;"
+  );
+  cursorS.setAttribute(
     "style",
     "top: " + e.pageY + "px; left: " + e.pageX + "px;"
   );
