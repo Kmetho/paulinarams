297924675f7830442e2f98fdb9534840a508ace2
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-03-28 11:47:39 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.addEventListener("mousemove", (e) => {
   );
 });
 
-if (areaHeight < 690) {
+if (window.innerHeight < 690) {
   const icons = document.querySelectorAll("i");
   icons.forEach((element) => {
     element.classList.remove("fa-2xl");
