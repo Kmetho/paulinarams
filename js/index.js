94f3a68beb3d111d6963b9f9c701c55354a3188d
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-06-26 10:57:24 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-const cursorB = document.querySelector(".big");
-const cursorS = document.querySelector(".small");
-
-document.addEventListener("mousemove", (e) => {
-  cursorB.setAttribute(
-    "style",
-    "top: " + e.pageY + "px; left: " + e.pageX + "px;"
-  );
-  cursorS.setAttribute(
-    "style",
-    "top: " + e.pageY + "px; left: " + e.pageX + "px;"
-  );
-});
-
 if (window.innerHeight < 690) {
   const icons = document.querySelectorAll("i");
   icons.forEach((element) => {
